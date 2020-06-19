d58f72ed7772b971277b580653b148156cd641b6
Pablo Calderón <49917052+pablinT@users.noreply.github.com>
2020-06-19 15:02:32 -0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,20 +1,14 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
-
-
-
 import pandas as pd
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 import time
 import re
 
 
-#browser =  webdriver.Chrome(executable_path=r"C:\Users\pablo\Downloads\chromedriver.exe")
 
 
 def recent_25_posts(username):
@@ -26,7 +20,7 @@ def recent_25_posts(username):
     browser.get(url)
     post = 'https://www.instagram.com/p/'
     post_links = []
-    while len(post_links) < 5:
+    while len(post_links) < 25:
         links = [a.get_attribute('href') for a in browser.find_elements_by_tag_name('a')]
         for link in links:
             if post in link and link not in post_links:
@@ -35,14 +29,14 @@ def recent_25_posts(username):
         browser.execute_script(scroll_down)
         time.sleep(5)
     else:
-        return post_links[:5]
+        return post_links[:25]
 
 
 
 def insta_details(urls):
 
-
-    """Take a post url and return post details"""
+    """para cada url extraemos los detalles"""
+    #la ubicacion del chromedriver.exe
     #browser = Chrome()
     browser =  webdriver.Chrome(executable_path=r"C:\Users\pablo\Downloads\chromedriver.exe")
 
@@ -50,51 +44,49 @@ def insta_details(urls):
     for link in urls:
         browser.get(link)
         try:
-        # This captures the standard like count. 
+        # extraemos los likes, si tu navegador está en otro idioma hay que cambiar el 'gusta' 
             likes = browser.find_element_by_partial_link_text('gusta').text
         except:
-        # This captures the like count for videos which is stored
+        # extraemos los likes de los videos
             xpath_view = '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/section[2]/div'
             likes = browser.find_element_by_xpath(xpath_view).text
+	# extraemos la antigüedad del post
         age = browser.find_element_by_css_selector('a time').text
-        xpath_comment = '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/div[1]/ul'
+        # ubicamos los comentarios y los extraemos
+	xpath_comment = '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/div[1]/ul'
         comment = browser.find_element_by_xpath(xpath_comment).text
         insta_link = link.replace('https://www.instagram.com/p','')
         post_details.append({'link': insta_link,'likes/views': likes,'age': age, 'comment': comment})
         time.sleep(10)
     return post_details
 
+#definimos la funcion que extrae los hashtags
 def find_hashtags(comment):
 	hashtags = re.findall('#[A-Za-z]+', comment)
 	return hashtags
 
-
+#definimos la funcion que limpia la columna de likes y los convierte en enteros
 def clean_likes(t):
 	x = re.split("\s", t, 1)
 	n_likes = int(x[0])
 	return n_likes
 
-
-
+#obtengo las url de los ultimos 25 posteos
 pablin_urls=recent_25_posts('pablintango')
 
+#evaluo la funcion en cada una de las 25 urls
 pablin_details=insta_details(pablin_urls)
 
-#print(pablin_urls)
-#print(pablin_details)
-
-############################################################
-# por que no funca esto?
-
+#construyo el dataframe
 pablin=pd.DataFrame(pablin_details)
+
+#creo la columna hashtags, extraigo los hashtags y convierto los likes en enteros
 pablin['hashtags']=pablin['comment'].apply(lambda x: find_hashtags(x))
 pablin['likes/views']=pablin['likes/views'].apply(lambda x: clean_likes(x))
 
-
+#ploteo los datos
 ax = pablin.plot.bar(x='age',y='likes/views', rot=0)
-
-# pablin.plot(y='likes/views', x='age',color='orange')
-# plt.xticks(rotation=60)
 plt.show()
 
+#exporto a un csv
 pablin.to_csv('informe_pablintango.csv')
